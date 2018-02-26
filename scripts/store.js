3442b14ecffb5ecd6586268a1a2659f02f3d7a01
Julie K <julieskim160@gmail.com>
2018-02-26 13:25:50 -0800

    update store to create, update, delete

--- scripts/store.js
+++ scripts/store.js
@@ -1,9 +1,10 @@
 'use strict';
-/* global cuid */
+/* global cuid, Item */
 const store = (function () {
   //items
   //hide
   //search
+  
   return {
     items: [
       { id: cuid(), name: 'apples', checked: false },
@@ -12,7 +13,38 @@ const store = (function () {
       { id: cuid(), name: 'bread', checked: false }
     ],
     hideCheckedItems: false,
-    searchTerm: ''
+    searchTerm: '',
+    findById:(id) => {
+      return store.items.find(items => items.id === id); 
+    },   
+    addItem:itemName => {
+      try {
+        Item.validateName(itemName);
+        //let test = Item.create(itemName);
+        store.items.push(Item.create(itemName));
+        console.log(test);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    findAndToggleChecked:(id) => {
+      this.findById(id).checked = !(this.findById(id).checked);
+    }, 
+    findAndUpdateName:(id, newName) => {
+      try {
+        Item.validateName();
+        store.items.findById(id).name = newName;
+      } catch(error) {
+        console.log('Cannot update name: {error.message}');
+      }
+    },
+    findAndDelete:(id) => {
+      store.items = store.items.filter(item => item.id !== id);
+      console.log(store.items);
+    },
+
+    
+
   };
   
 }() );
